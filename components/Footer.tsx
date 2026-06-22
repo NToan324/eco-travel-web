09ea8669378f8815728bbbb1f14e36b9b752ff3d
NToan324 <nhattoan664t@gmail.com>
2026-06-22 08:35:29 +0700

    [FiX]: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="w-full bg-white text-black border-t py-6 mt-auto">
       <div className="max-w-6xl mx-auto text-center">
-        <p>© {new Date().getFullYear()} Eco Travel. All rights reserved.</p>
+        <p>Sản phẩm NCKHSV - Nhóm 13 - GVHD: TS Nguyễn Minh Huân</p>
       </div>
     </footer>
   );
